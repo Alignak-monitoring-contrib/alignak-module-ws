a0f26647dfe63e42a0a64aff0e5c58f3a3532ad4
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-11-16 19:23:11 +0100

    Improve host creation tests

--- alignak_module_ws/ws.py
+++ alignak_module_ws/ws.py
@@ -85,6 +85,7 @@ class AlignakWebServices(BaseModule):
         :param mod_conf: module configuration file as a dictionary
         """
         BaseModule.__init__(self, mod_conf)
+        print("mod_conf: %s" % mod_conf.__dict__)
 
         # pylint: disable=global-statement
         global logger
@@ -414,6 +415,7 @@ class AlignakWebServices(BaseModule):
             post_data.pop('_id')
 
         logger.debug("post_data: %s", post_data)
+        print("post_data: %s" % post_data)
         return post_data
 
     def getHostsGroup(self, name, embedded=False):
